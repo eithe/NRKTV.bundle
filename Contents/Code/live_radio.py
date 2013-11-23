310517e5568c8f4a006451820923909e27803eff
eithe <eirik.hodne@gmail.com>
2013-11-23 22:51:32 +0100

    Alpha of live radio, seems to work fine on Plex web and Android clint, does not work good on Mac PHT

--- Contents/Code/live_radio.py
+++ Contents/Code/live_radio.py
@@ -13,50 +13,49 @@
 # NRK, if you are watching, don't hesitate to make contact.
 
 LIVE_RADIO_BASEURL = 'http://lyd.nrk.no/nrk_radio_'
-LIVE_RADIO_QUALITY = 'h' # h (high) or m (medium)
 
-LIVE_RADIO_STATIONS = [ \
-    ['p1_ostlandssendingen', u'P1', u'Den brede kanalen for folk flest. Norges største radiokanal. Bredt distriktstilbud.'],
-    ['p2', u'P2', u'Kulturkanalen med kunst, kultur, nyheter, debatt og samfunnsstoff.'],
-    ['p3', u'P3', u'Ungdomskanal med mye pop og rock-musikk, humor og skreddersydde nyheter for de unge.'],
-#    ['mp3', u'mPetre', u'Musikk for de yngre.'],
-#    ['klassisk', u'Klassisk', u'Klassisk musikk døgnet rundt'],
-#    ['alltid_nyheter', u'Alltid Nyheter', u'Hyppige nyhetsoppdateringer - BBC kveld/natt.'],
-#    ['sami', u'Sámi Radio', u'Tilbud for samisktalende.'],
-#    ['folkemusikk', u'Folkemusikk', u'Fra NRKs unike folkemusikkarkiv.'],
-#    ['jazz', u'Jazz', u'Jazz døgnet rundt.'],
-#    ['sport', u'Sport', u'Levende og arkivsport, engelsk fotball.'],
-#    ['p3_urort', u'P3 Urørt', u'Musikk.'],
-#    ['p3_pyro', u'P3 Pyro', u'Musikk.'],
-#    ['p3_radioresepsjonen', u'P3 Radioresepsjonen', u'Alltid Radioresepsjonen.'],
-#    ['gull', u'Gull', u'Godbiter fra arkivene.'],
-#    ['super', u'Super', u'Barnetilbud.'],
-#    ['p1_ostfold', u'P1 Østfold', u''],
-#    ['p1_buskerud', u'P1 Buskerud', u''],
-#    ['p1_sogn_og_fjordane', u'P1 Sogn og Fjordane', u''],
-#    ['p1_rogaland', u'P1 Rogaland', u''],
-#    ['p1_finnmark', u'P1 Finnmark', u''],
-#    ['p1_hedmark_og_oppland', u'P1 Hedmark og Oppland', u''],
-#    ['p1_hordaland', u'P1 Hordaland', u''],
-#    ['p1_more_og_romsdal', u'P1 Møre og Romsdal', u''],
-#    ['p1_nordland', u'P1 Nordland', u''],
-#    ['p1_telemark', u'P1 Telemark', u''],
-#    ['p1_troms', u'P1 Troms', u''],
-#    ['p1_trondelag', u'P1 Trøndelag', u''],
-#    ['p1_vestfold', u'P1 Vestfold', u''],
-#    ['p1_sorlandet', u'P1 Sørlandet', u'']
-]
+LIVE_RADIO_STATIONS = (
+    # Channel/image filename, name, description
+    ('p1_ostlandssendingen', u'P1', u'Den brede kanalen for folk flest. Norges største radiokanal. Bredt distriktstilbud.'),
+    ('p2', u'P2', u'Kulturkanalen med kunst, kultur, nyheter, debatt og samfunnsstoff.'),
+    ('p3', u'P3', u'Ungdomskanal med mye pop og rock-musikk, humor og skreddersydde nyheter for de unge.'),
+    ('mp3', u'mPetre', u'Musikk for de yngre.'),
+    ('klassisk', u'Klassisk', u'Klassisk musikk døgnet rundt'),
+    ('alltid_nyheter', u'Alltid Nyheter', u'Hyppige nyhetsoppdateringer - BBC kveld/natt.'),
+    ('sami', u'Sámi Radio', u'Tilbud for samisktalende.'),
+    ('folkemusikk', u'Folkemusikk', u'Fra NRKs unike folkemusikkarkiv.'),
+    ('jazz', u'Jazz', u'Jazz døgnet rundt.'),
+    ('sport', u'Sport', u'Levende og arkivsport, engelsk fotball.'),
+    ('p3_urort', u'P3 Urørt', u'Musikk.'),
+    ('p3_pyro', u'P3 Pyro', u'Musikk.'),
+    ('p3_radioresepsjonen', u'P3 Radioresepsjonen', u'Alltid Radioresepsjonen.'),
+    ('gull', u'Gull', u'Godbiter fra arkivene.'),
+    ('super', u'Super', u'Barnetilbud.'),
+    ('p1_ostfold', u'P1 Østfold', u''),
+    ('p1_buskerud', u'P1 Buskerud', u''),
+    ('p1_sogn_og_fjordane', u'P1 Sogn og Fjordane', u''),
+    ('p1_rogaland', u'P1 Rogaland', u''),
+    ('p1_finnmark', u'P1 Finnmark', u''),
+    ('p1_hedmark_og_oppland', u'P1 Hedmark og Oppland', u''),
+    ('p1_hordaland', u'P1 Hordaland', u''),
+    ('p1_more_og_romsdal', u'P1 Møre og Romsdal', u''),
+    ('p1_nordland', u'P1 Nordland', u''),
+    ('p1_telemark', u'P1 Telemark', u''),
+    ('p1_troms', u'P1 Troms', u''),
+    ('p1_trondelag', u'P1 Trøndelag', u''),
+    ('p1_vestfold', u'P1 Vestfold', u''),
+    ('p1_sorlandet', u'P1 Sørlandet', u'')
+)
 
-
+@route(pluginRoute('/radio_live'))
 def LiveRadioMenu():
     oc = ObjectContainer()
     
     # Adds all the station as track items
     for station in LIVE_RADIO_STATIONS:
-        url = '%s%s_mp3_%s' % \
-            (LIVE_RADIO_BASEURL, station[0], LIVE_RADIO_QUALITY)
+        url = '%s%s' % (LIVE_RADIO_BASEURL, station[0])
             
-        Log.Debug('NRK LiveRadioMenu: Added stream: %s' % url + ', station: %s' % station[1] + ', artist: %s' % station[2])
+        Log.Debug('NRK: Added stream: %s' % url + ', station: %s' % station[1])
         
         # Thumb file
         if station[0].startswith('nrk-p1'):
@@ -69,6 +68,6 @@ def LiveRadioMenu():
           url = url,
           artist=station[2],
           thumb=R('nrk-nettradio.png')
-        )) # TODO R(thumb_file)
+        )) # TODO, add all images for the various channels and then R(thumb_file)
     
     return oc
